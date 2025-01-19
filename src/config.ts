416ed36deafcc965ee9f5bb5025450e83e0e0776
Yogesh15063 <137427621+Yogesh15063@users.noreply.github.com>
2025-01-19 21:49:28 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 
-export const config = {
-    apiUrl: process.env.NODE_ENV === 'production'
-      ? 'https://chatbot-sa3r.onrender.com'
-      : 'http://localhost:3000', // Fallback to localhost if undefined
-  };
+// export const config = {
+//     apiUrl: process.env.NODE_ENV === 'production'
+//       ? 'https://chatbot-sa3r.onrender.com'
+//       : 'http://localhost:3000', // Fallback to localhost if undefined
+//   };
   
